1c70e16029c8fbe5cf043bae4b1b9025615abe3f
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-07-17 09:55:04 -0500

    Write main compression logic

---
+++ compresso/__main__.py
@@ -0,0 +1,34 @@
+import click
+
+from . import compress, decompress
+
+
+@click.group()
+def compresso():
+    pass
+
+
+# NEW CODE:
+compress_command = click.Command('compress',
+                                 callback=compress,
+                                 params=[
+                                     click.Argument(['filename'], type=click.Path(exists=True)),
+                                     click.Option(['--output', '-o'], type=click.Path(), default=None,
+                                                  help='Output file for compressed data.'),
+                                     click.Option(['--num-threads', '-T'], type=int, default=None,
+                                                  help='Number of threads to use for compression.'),
+                                     click.Option(['--worker-timeout', '-t'], type=int, default=None,
+                                                  help='Timeout for worker threads in seconds.'),
+                                     click.Option(['--max-rounds', '-r'], type=int, default=None,
+                                                    help='Maximum number of compression rounds to perform.')
+                                 ])
+decompress_command = click.Command('decompress',
+                                   callback=decompress,
+                                   params=[
+                                       click.Argument(['filename'], type=click.Path(exists=True))
+                                   ])
+compresso.add_command(compress_command)
+compresso.add_command(decompress_command)
+
+if __name__ == '__main__':
+    compresso()
